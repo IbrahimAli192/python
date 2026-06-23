3d9d1b357031e56e223625737d2ff6957cebbe34
IbrahimAli192 <the.honouredone663@gmail.com>
2026-06-23 18:44:01 +0300

    Add files via upload

---
+++ print_state.py
@@ -0,0 +1,12 @@
+# printing a greeting message
+print("hello world")
+
+# printing my name
+print("my name is ibrahim")
+
+# printing a fun fact about myself
+# ptint("i love playing soccer")
+print("i love playing soccer")
+
+# performing and printing a simple calculation
+print("the result of 3 + 5 is:", 3 + 5)
